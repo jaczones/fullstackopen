1e66e9122d72cd11fdf61525165fddf5040fb1ca
jaczones <jellybeanjones@twitter.com>
2021-04-15 23:30:30 -0600

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 
-
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
@@ -12,17 +11,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [count, setCount] = useState(0)
+  const average = (count / total).toFixed(2)
+  const positive = (good / total).toFixed(2)
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setTotal(total + 1)
+    setCount(count + 1)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setTotal(total + 1)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setTotal(total + 1)
+    setCount(count - 1 )
   }
 
   return (
@@ -37,6 +45,9 @@ const App = () => {
     <p>Good: {good}</p>
     <p>Neutral: {neutral}</p>
     <p>Bad: {bad}</p>
+    <p>All: {total}</p>
+    <p>Average: {average}</p>
+    <p>Positive: {positive}%</p>
     </div>
   )
 }
