a25118a54eb08bfe21fa854575892ea3db7508e4
jaczones <jellybeanjones@twitter.com>
2021-04-20 08:03:41 -0600

    2.0

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />    
+    </div>
+  )
+}
+
+
+export default App
+/* import React from 'react'
 
 const App = () => {
   const course = {
@@ -60,4 +98,4 @@ const Total = (props) => {
     </div>
   )
 }
-export default App
+export default App */
