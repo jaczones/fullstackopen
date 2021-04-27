e149a9154c8a754fa76ab9bd133a8dd808e406be
Zachariah Jones <zjones@twitter.com>
2021-04-27 08:26:21 -0600

    2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,22 +4,24 @@ import Person from './components/Person'
 const App = (props) => {
   const [ persons, setPersons ] = useState(props.persons) 
   const [ newName, setNewName ] = useState('')
-  
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName,
       id: persons.length + 1,
+      name: newName,
     }
+    if (persons.some(person => person.name === newName)){
+      alert(`${newName} is already in phonebook`)
+    } else {
     setPersons(persons.concat(personObject))
     setNewName('')
+    }
   }
 
   const handleSubmission = (event) => {
     //console.log(event.target.value)
     setNewName(event.target.value)
   }
-
   return (
     <div>
       <h2>Phonebook</h2>
