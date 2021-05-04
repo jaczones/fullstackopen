108fe82d71a19e925454e4d8d3924d70f88e1333
Zachariah Jones <zjones@twitter.com>
2021-05-04 09:34:10 -0600

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Form from './components/Form'
 import Search from './components/Search'
 import Filter from './components/Filter'
 import { findPerson } from './components/findPerson'
 
 const App = (props) => {
-  const [ persons, setPersons ] = useState(props.persons) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
   const filteredSearch = findPerson(persons, searchTerm)
   
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  },[])
+
   const handleFormSubmission = (event) => {
     event.preventDefault()
     const personObject = {
