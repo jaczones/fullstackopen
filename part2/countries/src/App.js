f2fd2c659e711abfac3e8a7e2c6196b84fb4cff9
jaczones <jellybeanjones@twitter.com>
2021-05-28 08:32:43 -0600

    starting weather component

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Filter, FilterLogic } from './components/Filter'
+import Country from './components/Country'
 
 const App = () => {
   const [countries, setCountries] = useState([])
   const [allCountries, setAllCountries] = useState([])
   const [newFilter, setNewFilter] = useState('')
-
+  const [weather, setWeather] = useState([])
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
@@ -14,6 +15,22 @@ const App = () => {
         setAllCountries(response.data)
       })
   }, [])
+
+  useEffect(() => {
+    const params = {
+      access_key: process.env.REACT_APP_WEATHER_KEY,
+      query: 'France'
+    }
+    axios.get('http://api.weatherstack.com/current', {params})
+      .then(response => {
+        console.log(response)
+        const apiResponse = response.data;
+        console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
+      }).catch(error => {
+        console.log(error);
+      });
+  })
+  
 
   const handleFilterChange = (event) => {
     setNewFilter(event.target.value)
