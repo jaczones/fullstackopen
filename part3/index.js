765795a241d5f65b628149bdd2f891825350962f
Zachariah Jones <zjones@twitter.com>
2021-06-29 08:09:15 -0600

    part 3

---
+++ part3/index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons =  [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Zac Jones",
+      "number": "8888888",
+      "id": 2
+    },
+    {
+      "name": "Zac",
+      "number": "4574574",
+      "id": 3
+    }
+  ]
+
+
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+
+/*   app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)  
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
+  })
+
+app.delete('api/notes/:id', (request, resopnse) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+}) */
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
