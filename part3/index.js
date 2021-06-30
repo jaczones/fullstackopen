99ae05bc7401529075d828326d205bdde86c06e9
Zachariah Jones <zjones@twitter.com>
2021-06-30 17:41:02 -0600

    3.8

--- part3/index.js
+++ part3/index.js
@@ -28,7 +28,11 @@ const info = {
   date : date
 }
 
-  app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+  morgan.token('body', function (req, res) {
+     return JSON.stringify(req.body) 
+    })
+
+  app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
   app.get('/api/persons', (request, response) => {
     response.json(persons)
