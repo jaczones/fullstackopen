0e340021db5e746a965a2ccf8d8025ae5b7309e9
Zachariah Jones <zjones@twitter.com>
2021-06-30 08:36:23 -0600

    3.6

--- part3/index.js
+++ part3/index.js
@@ -67,10 +67,15 @@ const generateId = () => {
 
   app.post('/api/persons', (request, response) => {
     const body = request.body
-
-    if (!body.name) {
+    const personMatch = persons.find((p) => p.name === body.content)
+    if (!body.name || !body.number) {
       return response.status(400).json({ 
         error: 'content missing' 
+      })
+    }
+    if (personMatch){
+      return response.status(400).json({
+        error: 'name already exists in phonebook'
       })
     }
 
@@ -79,47 +84,9 @@ const generateId = () => {
     number: body.number,
     id: generateId(),
     }
-
     persons = persons.concat(person)
-
     response.json(person)
   })
-/*   
-
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-  
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
-  })
-
-app.delete('api/notes/:id', (request, resopnse) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
-}) */
 
 const PORT = 3001
 app.listen(PORT, () => {
