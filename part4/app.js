a4ca450f0ea7fd176cbafed8ae16b9f18dda1f47
Zachariah Jones <zjones@twitter.com>
2021-07-16 14:43:51 -0600

    4.3

--- part4/app.js
+++ part4/app.js
@@ -6,12 +6,12 @@ const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 app.use(cors())
 app.use(express.json())
