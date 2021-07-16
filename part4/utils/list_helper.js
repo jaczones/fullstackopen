776c126aa0119cacbf3201de006a7d9cb24f512c
Zachariah Jones <zjones@twitter.com>
2021-07-16 15:36:55 -0600

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
     if (blogs) {
         return 1
@@ -11,8 +13,17 @@ const totalLikes = (blogs) => {
             ? blogs[0].likes
             : 0
 }
+
+const faveBlog = (blogs) => {
+    return blog
+        ? blogs.reduce((faveBlog, blog) => blog.liks > faveBlog.likes ? blog : faveBlog)
+        : blogs.length === 1
+            ? blogs[0]
+            : null
+}
   
 module.exports = {
     dummy,
     totalLikes,
+    faveBlog
 }
