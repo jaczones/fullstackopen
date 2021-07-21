e6ff1b3d8d12a599888b7e8ccc53f19db7b96b62
Zachariah Jones <zjones@twitter.com>
2021-07-21 16:19:20 -0600

    5.3

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import BlogForm from './components/BlogForm.js'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login' 
 
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('') 
+  const [successMessage, setSuccessMessage] = useState(null)
   const [user, setUser] = useState(null)
 
 
@@ -51,6 +54,30 @@ const App = () => {
   const handleLogout = async (event) => {
     window.localStorage.clear()
   }
+
+  const createBlog = async (newBlog) => {
+    try {
+      const createdBlog = await blogService
+        .create(newBlog)
+      setSuccessMessage(
+        `Blog ${newBlog.title} was successfully added`
+      )
+      setBlogs(blogs.concat(createdBlog))
+      setErrorMessage(null)
+      setTimeout(() => {
+        setSuccessMessage(null)
+      }, 5000)
+    } catch(exception) {
+      setErrorMessage(
+        `Cannot add blog ${newBlog.title}`
+      )
+      setSuccessMessage(null)
+      setTimeout(() => {
+        setSuccessMessage(null)
+      }, 5000)
+    }
+  }
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
@@ -78,10 +105,14 @@ const App = () => {
 
   return (
     <div>
+      <Notification errorMessage={errorMessage} successMessage={successMessage} />
       {user === null ?
         loginForm() :
         <div>
         <p>{user.name} logged-in <form onSubmit={handleLogout}><button type='submit'>logout</button></form></p>
+        <BlogForm
+            createBlog={createBlog}
+        />
         <h2>blogs</h2>
         {blogs.map(blog =>
       <Blog key={blog.id} blog={blog} />
