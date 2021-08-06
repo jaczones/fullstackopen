0ea9b94d32ff7c79c54548ee865ea1193f36e4cf
Zachariah Jones <zjones@twitter.com>
2021-08-06 12:21:17 -0600

    6.13

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import NewAnecdote from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import VisibilityFilter from './components/VisibilityFilter'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
-
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+    .getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+  }, [dispatch])
   return (
     <div>
       <h2>Anecdotes</h2>
