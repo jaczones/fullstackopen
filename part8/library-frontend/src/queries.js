6cbbea86ed4ae2593dc16bc84a0d4425960236e9
Zachariah Jones <zjones@twitter.com>
2021-09-01 17:28:32 -0600

    8.9

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -10,3 +10,38 @@ query {
 }
 `
 
+export const ALL_BOOKS = gql`
+query {
+  allBooks {
+    title
+    published
+    author
+  }
+}
+`
+
+export const CREATE_BOOK = gql`
+mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!){
+  addBook(
+    title: $title,
+    author: $author,
+    published: $published,
+    genres: $genres
+  ) {
+    title,
+    author
+  }
+}
+`
+
+export const EDIT_BORN_YEAR = gql`
+mutation changeBornYear($name: String!, $setBornTo: Int!){
+  editAuthor(
+    name: $name, 
+    setBornTo: $setBornTo
+    ) {
+      name
+      born
+  }
+}
+`
