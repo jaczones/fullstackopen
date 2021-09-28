37599bd1b10e6e50b65fe4a95f7d28d961416698
Zachariah Jones <zjones@twitter.com>
2021-09-28 08:49:29 -0600

    9.1

---
+++ part9/typescript-exercises/bmiCalculator.ts
@@ -0,0 +1,21 @@
+//The BMI is defined as the body mass divided by the square of the body height
+
+type Result = string;
+
+const bmiCalculator = (height: number, mass: number) : Result => {
+  let m = height/100;
+  let h = m * m;
+  let bmi = mass/h;
+  console.log(bmi)
+  if (bmi < 18.5) {
+    return "Underweight";
+  }
+  else if (bmi < 24.9) {
+    return "Normal (healthy weight)"
+  }
+  else if (bmi < 29) {
+    return "Overweight"
+  } return "Obese"
+}
+
+console.log(bmiCalculator(180, 74));
